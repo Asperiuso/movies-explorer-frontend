15a7bdd4226c88ff4cff04df13bbab69a87d8897
Asperius <banansmth@gmail.com>
2023-11-15 01:03:26 +0300

    fix: Подключение к серверу

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,6 +9,7 @@ function Footer() {
     location.pathname === '/saved-movies' ?
     (
       <footer className="footer">
+
         <h2 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
         <div className="footer__data">
           <p className="footer__copyright">&copy; 2023</p>
@@ -17,6 +18,7 @@ function Footer() {
             <li><a href="https://github.com/Asperiuso/movies-explorer-frontend" className="footer__link link" target="_blank" rel="noreferrer">Github</a></li>
           </ul>
         </div>
+
       </footer>
     )
     :
